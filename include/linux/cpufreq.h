84b75cde17da0ac171002c91842d96f5f0c3a28f
DooMLoRD <metoo.mememe@gmail.com>
2014-05-10 19:52:24 +0530

    cpufreq: Add LagFree gov (thanks to @androdguide)
    Signed-off-by: Omar Einea <eineao@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -396,6 +396,9 @@ extern struct cpufreq_governor cpufreq_gov_adaptive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSV2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
+extern struct cpufreq_governor cpufreq_gov_lagfree;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
 #endif
 
 
