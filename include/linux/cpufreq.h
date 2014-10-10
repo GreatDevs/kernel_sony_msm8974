cb102d3a6523b942c7eb4e47ab31b9ef09824f96
Francis <trinityhaxxorx@gmail.com>
2014-10-10 11:19:53 +0300

    cpufreq: Add Adaptive gov
    Signed-off-by: Omar Einea <eineao@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -390,6 +390,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
+extern struct cpufreq_governor cpufreq_gov_adaptive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_adaptive)
 #endif
 
 
